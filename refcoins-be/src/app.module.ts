df889091c2ec057cb7219b7a78c6cba1bbbe3ff2
DfFernando <hdilshan.fernando11@gmail.com>
2024-05-22 23:36:08 +0530

    nest prisma module

--- refcoins-be/src/app.module.ts
+++ refcoins-be/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PropertyModule } from './property/property.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PropertyModule } from './property/property.module';
 
 @Module({
-  imports: [],
+  imports: [PropertyModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
